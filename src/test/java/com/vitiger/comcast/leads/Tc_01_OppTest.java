0b530d55e13a3a214a8a962bcd2f6e1b51f0d072
AnupDK26 <anupdk26@gmail.com>
2021-09-03 00:23:15 +0530

    push practice1

---
+++ src/test/java/com/vitiger/comcast/leads/Tc_01_OppTest.java
@@ -0,0 +1,19 @@
+package com.vitiger.comcast.leads;
+
+import org.testng.annotations.Test;
+
+public class Tc_01_OppTest {
+
+	@Test
+	public void OppTest() {
+
+		System.out.println("Hi");
+		
+	}
+	
+	@Test
+	public void Opp2Test() {
+		System.out.println("hiiiiii");
+	}
+
+}
---
+++ src/test/java/com/vitiger/comcast/leads/Tc_02_LeadTest.java
@@ -0,0 +1,19 @@
+package com.vitiger.comcast.leads;
+
+import org.testng.annotations.Test;
+
+public class Tc_02_LeadTest {
+
+	@Test
+	public void leadTest() {
+	
+		System.out.println("Hello");
+		
+	}
+	
+	@Test
+	public void lead2Test() {
+		System.out.println("Hellooooooo");
+	}
+
+}
