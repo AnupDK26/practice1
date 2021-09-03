82faf727a94b3ac604efbb375c62a241f8305b4a
AnupDK26 <anupdk26@gmail.com>
2021-09-03 10:15:22 +0530

    Practice

---
+++ src/test/java/com/vitiger/comcast/leads/OppTest.java
@@ -0,0 +1,11 @@
+package com.vitiger.comcast.leads;
+
+public class OppTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Helooooo");
+	}
+
+}
